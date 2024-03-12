49c244c49b6f8f99ea3c2d131df7e5b86c37a696
Nate Johnson <njohnson2897@gmail.com>
2024-03-12 08:54:39 -0500

    adds jquery library and now stores entirety of api request response to local storage rather than just city name

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
 const citySearchBtn = document.querySelector('#citySearchBtn')
+const mainCard = document.querySelector('#current-forecast')
+const dayOne = document.querySelector('#forecast-day-one')
+const dayTwo = document.querySelector('#forecast-day-two')
+const dayThree = document.querySelector('#forecast-day-three')
+const dayFour = document.querySelector('#forecast-day-four')
+const dayFive = document.querySelector('#forecast-day-five')
 
 function handleSearchSubmit(event) {
     event.preventDefault();
@@ -11,12 +17,13 @@ function handleSearchSubmit(event) {
     } else {
         console.log(`User searched for ${cityInputVal}`)
         apiRequestCurrent();
-        apiRequestFiveDay();
+        // apiRequestFiveDay();
+        printForecast();
     };
 
-    const cities = readLocalStorage();
-    cities.push(cityInputVal);
-    saveToLocalStorage(cities);
+    // const cities = readLocalStorage();
+    // cities.push(cityInputVal);
+    // saveToLocalStorage(cities);
 };
 
 function apiRequestCurrent() {
@@ -27,24 +34,28 @@ function apiRequestCurrent() {
             return response.json()
         .then(function (data) {
             console.log(data)
+            const cities = readLocalStorage();
+            cities.push(data);
+            saveToLocalStorage(cities);
         });
     });
 };
 
-function apiRequestFiveDay() {
-    const cityInputVal = document.querySelector('#cityInput').value;
-    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputVal}&appid=8c387a43d44b729cc8e9f5084ed67cad`
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json()
-        .then(function (data) {
-            console.log(data)
-        });
-    });
-};
+// function apiRequestFiveDay() {
+//     const cityInputVal = document.querySelector('#cityInput').value;
+//     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputVal}&appid=8c387a43d44b729cc8e9f5084ed67cad`
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json()
+//         .then(function (fiveDayData) {
+//             console.log(fiveDayData)
+//             return fiveDayData;
+//         });
+//     });
+// };
 
 function  readLocalStorage() {
-    let  cities = JSON.parse(localStorage.getItem('cities'))
+    let  cities = JSON.parse(localStorage.getItem('cities'));
 
     if(!cities){
         cities = [];
@@ -55,6 +66,15 @@ function  readLocalStorage() {
 function saveToLocalStorage(cities) {
     localStorage.setItem('cities', JSON.stringify(cities));
 }
+
+// function printForecast() {
+//     const cities = readLocalStorage();
+//     const cityNameDate = $('<h2>').text(cities[0].name.value);
+//     const cityTemp = $('<p>').text(cities[0].main.temp.value);
+//     const cityWind = $('<p>').text(cities[0].wind.speed.value);
+//     const cityHumid = $('<p>').text(cities[0].main.humidity.value);
+//     mainCard.append(cityNameDate, cityTemp, cityWind, cityHumid);
+// }
 
 
 
